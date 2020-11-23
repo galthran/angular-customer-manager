008f548810c17eb0adbe082ec2b09fd20415067d
galthran80 <rafal.jarosinski@gmail.com>
2020-11-23 20:36:15 +0100

    class learning

---
+++ sandbox/classes.ts
@@ -0,0 +1,53 @@
+interface CanTalk {
+    talk(): string;
+    test(): any;
+}
+
+
+class Person implements CanTalk{
+    //protected name: string;
+    //private age: number;
+
+    constructor(protected name: string, public age: number) {
+        //this.name = name;
+        //this.age = age;
+    }
+
+    talk(): string {
+        return 'bla bla bla';
+    }
+    test() {
+        console.log("Run method test");
+    }
+
+    welcome () {
+        console.log(`Welcome ${this.name}`);
+    }
+
+    isAdult() {
+        if(this.age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Vendor extends Person {
+    advisor: string;
+
+    constructor(name: string, age: number, advisor: string) {
+        super(name, age);
+        this.advisor = advisor;
+    }
+
+    welcome () {
+        return `Good morning ${this.name}`;
+    }
+}
+
+let vendor = new Vendor("Piotr", 22, "Agata");
+vendor.welcome();
+
+console.log(vendor);
+console.log(vendor.isAdult());
